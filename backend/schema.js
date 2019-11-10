fe298ba158e20399e71756c4e53b86c5f279d6e3
DerpyCrabs <derpycrabs@gmail.com>
2019-11-10 18:33:24 +0700

    backend: PoC work queue schema

--- backend/schema.js
+++ backend/schema.js
@@ -3,13 +3,42 @@ const typeDefs = `
     email: String!
     role: String!
   }
+  type WorkResult {
+    result: String!
+  }
+  type WorkType {
+    id: Int!
+  }
+  enum WorkStage {
+    WaitingForCompilation
+    Compilation
+    WaitingForRunning
+    Running
+    Done
+  }
+  type Work {
+    id: Int!
+    language: String!
+    type: WorkType!
+    stage: WorkStage!
+    result: WorkResult
+  }
+  type WorkQueue {
+    queue: [Work!]!
+  }
+  type WorkQueueMutation {
+    add_work(language: String!, text: String!): Int!
+    check_work_result(id: Int!): Boolean!
+  }
   type Query {
     me: User
     users: [User!]
+    work_queue: WorkQueue!
   }
   type Mutation {
     signup (email: String!, password: String!): String
     login (email: String!, password: String!): String
+    work_queue: WorkQueueMutation!
   }
 `
 module.exports = typeDefs
