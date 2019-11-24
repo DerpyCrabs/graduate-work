1997f5502747d8ca178d2d2c005d1d1797b3be1a
DerpyCrabs <derpycrabs@gmail.com>
2019-11-24 16:35:59 +0700

    backend: plugins schema

--- backend/schema.js
+++ backend/schema.js
@@ -6,8 +6,22 @@ const typeDefs = `
   type WorkResult {
     result: String!
   }
+  type PluginSetting {
+    key: String!
+    value: String!
+  }
+  type PluginInfo {
+    id: Int!
+    enabled: Boolean!
+    name: String!
+    stage: String!
+    version: String!
+    settings: [PluginSetting!]!
+    stats: [String!]
+  }
   type WorkType {
     id: Int!
+    pluginQueue: [PluginInfo!]!
   }
   enum WorkStage {
     WaitingForCompilation
@@ -30,15 +44,22 @@ const typeDefs = `
     add_work(language: String!, type_id: Int!, text: String!): Int!
     check_work_result(id: Int!): Boolean!
   }
+  type PluginMutation {
+    set_setting(id: Int!, key: String!, value: String!): PluginSetting
+    enable_plugin: String
+    disable_plugin: String
+  }
   type Query {
     me: User
     users: [User!]
     work_queue: WorkQueue!
+    plugins: [PluginInfo!]!
   }
   type Mutation {
     signup (email: String!, password: String!): String
     login (email: String!, password: String!): String
     work_queue: WorkQueueMutation!
+    plugin (id: Int!): PluginMutation!
   }
 `
 module.exports = typeDefs
