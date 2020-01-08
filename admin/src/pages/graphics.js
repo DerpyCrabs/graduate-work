1fb73fa1cbfc17c8fc7972de71b505213b3b945e
DerpyCrabs <derpycrabs@gmail.com>
2020-01-08 23:30:18 +0700

    admin: add graphics page scaffolding

--- admin/src/pages/graphics.js
+++ admin/src/pages/graphics.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import {
+  Paper,
+  Typography,
+  Card,
+  CardHeader,
+  CardContent
+} from '@material-ui/core'
 
 export default function Graphics() {
   const allowed = JSON.parse(localStorage.getItem('graphics'))
   if (!allowed) {
     return <div>Graphics are not allowed by your license</div>
   }
-  return <div>Graphics</div>
+  return (
+    <div style={{ padding: 5, display: 'flex', flexDirection: 'column' }}>
+      <Card style={{ margin: 5 }}>
+        <CardHeader title='Language usage' />
+        <CardContent>lang usage</CardContent>
+      </Card>
+      <Card style={{ margin: 5 }}>
+        <CardHeader title='Test completion' />
+        <CardContent>lang usage</CardContent>
+      </Card>
+      <Card style={{ margin: 5 }}>
+        <CardHeader title='Student activity' />
+        <CardContent>lang usage</CardContent>
+      </Card>
+      <Card style={{ margin: 5 }}>
+        <CardHeader title='Average plugin execution time' />
+        <CardContent>lang usage</CardContent>
+      </Card>
+    </div>
+  )
 }
